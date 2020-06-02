5280413dbba5ede9f7a531929d54926fb47da59d
Ashwin-DevAsh <2022ashwin@gmail.com>
2020-06-03 03:21:19 +0530

    bugs

--- Wallet-Transaction/main.go
+++ Wallet-Transaction/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"encoding/json"
-	"io/ioutil"
 	"log"
 	"net/http"
 	"strconv"
@@ -61,20 +60,10 @@ func handelRequest() {
 
 			log.Println(request.Body)
 
-			// err := json.NewDecoder(request.Body).Decode(&transactionData)
-
-			body, err := ioutil.ReadAll(request.Body)
-			if err != nil {
-				panic(err)
-			}
-			log.Println(string(body))
-			err = json.Unmarshal(body, &transactionData)
-			if err != nil {
-				panic(err)
-			}
+			err := json.NewDecoder(request.Body).Decode(&transactionData)
 
 			if err != nil {
-				log.Println("error = ", err)
+				log.Printf("verbose error info: %#v", err)
 				return
 			}
 
