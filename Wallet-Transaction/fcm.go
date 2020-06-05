6834874f66007056752534f0f5c51d9dfc8845de
Ashwin-DevAsh <2022ashwin@gmail.com>
2020-06-06 01:17:18 +0530

    fix notification bugs

--- Wallet-Transaction/fcm.go
+++ Wallet-Transaction/fcm.go
@@ -7,7 +7,7 @@ import (
 )
 
 func sendNotification(devices []string, data map[string]string) {
-	const serverKey 
+	var serverKey 
 	      := "AAAAwveu2fw:APA91bFuqXWjuuTBix0mRNydlB3o2hEp9Adky7IJX2LNS3mKvkblUCtbeqGFUWrjRCgyrwRY-Q46b_M6weSf0wxj33wv7h_ASrpQnSQmWwRVEEun0T3lrliTh2NhQNYHypkeM38gjI9A"
 	c := fcm.NewFcmClient(serverKey)
 	c.AppendDevices(devices)
