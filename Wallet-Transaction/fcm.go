13b0b486fceeb6ec1c9d0d2178afc68284533878
Ashwin-DevAsh <2022ashwin@gmail.com>
2020-06-07 23:27:36 +0530

    change notifications

--- Wallet-Transaction/fcm.go
+++ Wallet-Transaction/fcm.go
@@ -6,20 +6,14 @@ import (
 	"github.com/NaySoftware/go-fcm"
 )
 
-type PaymentData struct{
-	Type string,
-	amount string
-}
-
-func sendNotification(devices []string, from string) {
+func sendNotification(devices []string, from string, amount string) {
 	var serverKey string = "AAAAwveu2fw:APA91bFuqXWjuuTBix0mRNydlB3o2hEp9Adky7IJX2LNS3mKvkblUCtbeqGFUWrjRCgyrwRY-Q46b_M6weSf0wxj33wv7h_ASrpQnSQmWwRVEEun0T3lrliTh2NhQNYHypkeM38gjI9A"
 
 	log.Println("Notification send to ", devices)
 	c := fcm.NewFcmClient(serverKey)
 	c.AppendDevices(devices)
-
-	c.NewFcmRegIdsMsg(devices, paymentData{
-	    "receivedMoney",from
+	c.NewFcmRegIdsMsg(devices, map[string]string{
+		"type": "receivedMoney," + from + ",",
 	})
 	c.Send()
 }
