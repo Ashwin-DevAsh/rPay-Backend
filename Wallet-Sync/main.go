2ae91f58dbf1d7aeb6684821689f41cee1a6089e
Ashwin-DevAsh <2022ashwin@gmail.com>
2020-06-11 22:25:29 +0530

    add user event

--- Wallet-Sync/main.go
+++ Wallet-Sync/main.go
@@ -61,11 +61,15 @@ func main() {
 
 	server.OnEvent("/", "newUser", func(s socketio.Conn, data map[string]string) {
 		log.Println("add user = ", data)
+
 		if data != nil {
 			s.Join("all")
 			s.Join(data["number"])
 			updateOnline(db, data["number"], s.ID(), data["fcmToken"], true)
-			server.BroadcastToRoom("/", "all", "addNewUser", data)
+			server.BroadcastToRoom("/", "all", "addNewUser", map[string]string{
+				"number": data["number"],
+				"amount": data["amount"],
+			})
 		}
 		s.Emit("doUpdate")
 
