110cf94355f071dd3564fde51f6d8138dd8da898
Ashwin-DevAsh <2022ashwin@gmail.com>
2020-07-17 23:12:18 +0530

    changed api key

--- Wallet-Auth/Routes/Auth.js
+++ Wallet-Auth/Routes/Auth.js
@@ -3,7 +3,7 @@ const Otp = require("../Schemas/otp");
 const https = require("https");
 const User = require("../Schemas/users");
 const jwt = require("jsonwebtoken");
-const api_key_otp = "b614c4f7-c046-11ea-9fa5-0200cd936042";
+const api_key_otp = "a04d885e-c801-11ea-9fa5-0200cd936042";
 
 //change database
 
