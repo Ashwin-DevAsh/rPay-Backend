3c53832061c729adcc9fc23e83b28d60457b1c15
Ashwin-DevAsh <2022ashwin@gmail.com>
2020-07-23 15:37:18 +0530

    add docker

--- Wallet-Admin/Database/Connections/connection.js
+++ Wallet-Admin/Database/Connections/connection.js
@@ -1,10 +1,15 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
-mongoose.connect('mongodb://localhost',{useNewUrlParser:true,useUnifiedTopology:true},).then(()=>{
-    console.log("suc")
-})
-.catch(()=>{
-    console.log("Fail")
-})
+mongoose
+  .connect("mongodb://auth-database/", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("suc");
+  })
+  .catch(() => {
+    console.log("Fail");
+  });
 
-module.exports = mongoose
+module.exports = mongoose;
