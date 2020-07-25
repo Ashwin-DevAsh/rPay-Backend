09cfb6a79cf7a288d762f0e20b0239e029812c6f
Ashwin-DevAsh <2022ashwin@gmail.com>
2020-07-25 20:49:07 +0530

    add users

--- Wallet-Admin/Routes/Users.js
+++ Wallet-Admin/Routes/Users.js
@@ -4,15 +4,23 @@ const postgres = require("../Database/Connections/pgConnections");
 const Users = require("../Database/Schema/Users");
 
 app.get("/getUsers", (req, res) => {
+  var token = req.get("token");
   console.log("Getting Users");
-  Users.find({})
-    .exec()
-    .then((doc) => {
-      res.send(doc);
-    })
-    .catch((e) => {
-      res.send({ err: e });
-    });
+
+  jwt.verify(token, process.env.PRIVATE_KEY, function (err, decoded) {
+    if (err) {
+      res.send({ message: "error", err });
+    } else {
+      Users.find({})
+        .exec()
+        .then((doc) => {
+          res.send(doc);
+        })
+        .catch((e) => {
+          res.send({ err: e });
+        });
+    }
+  });
 });
 
 module.exports = app;
