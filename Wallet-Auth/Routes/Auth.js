6310e487154564f499fec03507ab6518c054a2f9
Ashwin-DevAsh <2022ashwin@gmail.com>
2020-07-28 21:22:32 +0530

    add merchant endpoint

--- Wallet-Auth/Routes/Auth.js
+++ Wallet-Auth/Routes/Auth.js
@@ -8,67 +8,9 @@ var api = require("../node_modules/clicksend/api.js");
 var smsMessage = new api.SmsMessage();
 //change database
 
-app.get("/getOtp", (req, res) => {
-  var number = req.query["number"];
-  var otpNumber = Math.floor(1000 + Math.random() * 9000);
-  if (number) {
-    Otp.deleteMany({ number })
-      .exec()
-      .then(() => {
-        smsMessage.from = "Rpay";
-        smsMessage.to = `+${number}`;
-        smsMessage.body = `<#> Rpay never calls you asking for otp. Sharing it with anyone gives them full access to your Rpay wallet. Your Login OTP is ${otpNumber} . ID: uPxbwGuwMaB`;
+app.get("/getOtp", (req, res) => sendOtp(req, res, "uPxbwGuwMaB"));
 
-        var smsApi = new api.SMSApi(
-          "ashwin.r.2018.cse@rajalakshmi.edu.in",
-          "49EB7110-46BA-6A73-9DDE-15C7B2F9C9F0"
-        );
-
-        var smsCollection = new api.SmsMessageCollection();
-
-        smsCollection.messages = [smsMessage];
-
-        smsApi
-          .smsSendPost(smsCollection)
-          .then(function (response) {
-            console.log(response.body);
-            const otpObject = new Otp({
-              number,
-              otp: otpNumber,
-              verified: false,
-            });
-            otpObject.save();
-            res.json([{ message: "done" }]);
-          })
-          .catch(function (err) {
-            console.error(err.body);
-            res.json([{ message: "failed" }]);
-          });
-
-        // https
-        //   .get(
-        //     `https://2factor.in/API/V1/${api_key_otp}/SMS/%2B${number}/${otpNumber}`
-        //   )
-        //   .on("finish", () => {
-        //     const otpObject = new Otp({
-        //       number,
-        //       otp: otpNumber,
-        //       verified: false,
-        //     });
-        //     otpObject.save();
-        //     res.json([{ message: "done" }]);
-        //   })
-        //   .on("error", () => {
-        //     res.json([{ message: "failed" }]);
-        //   });
-      })
-      .catch((err) => {
-        res.json([{ message: "failed" }]);
-      });
-  } else {
-    res.json([{ message: "failed" }]);
-  }
-});
+app.get("/getOtpMerchant", (req, res) => sendOtp(req, res, "RGhR1jQhc7+"));
 
 app.post("/setOtp", (req, res) => {
   var otpNumber = req.body["otpNumber"];
@@ -139,5 +81,50 @@ app.delete("/deleteOtp", (req, res) => {
     res.json([{ message: "missing parameter" }]);
   }
 });
+
+var sendOtp = (req, res, appId) => {
+  var number = req.query["number"];
+  var otpNumber = Math.floor(1000 + Math.random() * 9000);
+  if (number) {
+    Otp.deleteMany({ number })
+      .exec()
+      .then(() => {
+        smsMessage.from = "Rpay";
+        smsMessage.to = `+${number}`;
+        smsMessage.body = `<#> Rpay never calls you asking for otp. Sharing it with anyone gives them full access to your Rpay wallet. Your Login OTP is ${otpNumber} . ID: ${appId}`;
+
+        var smsApi = new api.SMSApi(
+          "ashwin.r.2018.cse@rajalakshmi.edu.in",
+          "49EB7110-46BA-6A73-9DDE-15C7B2F9C9F0"
+        );
+
+        var smsCollection = new api.SmsMessageCollection();
+
+        smsCollection.messages = [smsMessage];
+
+        smsApi
+          .smsSendPost(smsCollection)
+          .then(function (response) {
+            console.log(response.body);
+            const otpObject = new Otp({
+              number,
+              otp: otpNumber,
+              verified: false,
+            });
+            otpObject.save();
+            res.json([{ message: "done" }]);
+          })
+          .catch(function (err) {
+            console.error(err.body);
+            res.json([{ message: "failed" }]);
+          });
+      })
+      .catch((err) => {
+        res.json([{ message: "failed" }]);
+      });
+  } else {
+    res.json([{ message: "failed" }]);
+  }
+};
 
 module.exports = app;
