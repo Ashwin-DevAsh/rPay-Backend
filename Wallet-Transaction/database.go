a22adb1f4242c095e99169136675ad0b3e50543f
Ashwin-DevAsh <2022ashwin@gmail.com>
2020-07-29 01:33:47 +0530

    socket id

--- Wallet-Transaction/database.go
+++ Wallet-Transaction/database.go
@@ -90,8 +90,8 @@ func doTransaction(db *sql.DB, from string, fromName string, to string, toName s
 	dt := time.Now()
 
 	_, errTrans :=
-		tx.Exec("insert into transactions(transactionTime,fromID,toID,toName,amount,fromName,isGenerated) values($1,$2,$3,$4,$5,$6,$7)",
-			dt.Format("01-02-2006 15:04:05"), from, to, toName, amount, fromName,false)
+		tx.Exec("insert into transactions(transactionTime,fromID,toID,toName,amount,fromName,isGenerated,iswithdraw) values($1,$2,$3,$4,$5,$6,$7)",
+			dt.Format("01-02-2006 15:04:05"), from, to, toName, amount, fromName,false,false)
 
 	if errTrans != nil {
 		tx.Rollback()
@@ -126,8 +126,8 @@ func addMoney(db *sql.DB, from string, fromName string, to string, toName string
 	}
 	dt := time.Now()
 	_, errTrans :=
-		tx.Exec("insert into transactions(transactionTime,fromID,toID,toName,amount,fromName,isGenerated) values($1,$2,$3,$4,$5,$6,$7)",
-			dt.Format("01-02-2006 15:04:05"), from, to, toName, amount, fromName,true)
+		tx.Exec("insert into transactions(transactionTime,fromID,toID,toName,amount,fromName,isGenerated,iswithdraw) values($1,$2,$3,$4,$5,$6,$7)",
+			dt.Format("01-02-2006 15:04:05"), from, to, toName, amount, fromName,true,false)
 
 	if errTrans != nil {
 		tx.Rollback()
