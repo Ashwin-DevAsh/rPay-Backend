5319824d160187f0dddb1d6f4554224822fd7042
Ashwin-DevAsh <2022ashwin@gmail.com>
2020-07-29 21:29:01 +0530

    rfactor code

--- Wallet-Admin/Routes/Stats.js
+++ Wallet-Admin/Routes/Stats.js
@@ -40,11 +40,9 @@ function doProcess(req, res, queryFunction) {
         var day = await postgres.query(queryFunction(days), ["day"]);
         var week = await postgres.query(queryFunction(days), ["week"]);
         var month = await postgres.query(queryFunction(days), ["month"]);
-        if (days == 1) {
-          var hour = await postgres.query(queryFunction(days), ["hour"]);
-        } else {
-          var hour = day;
-        }
+
+        var hour = await postgres.query(queryFunction(days), ["hour"]);
+
         console.log(hour.rows);
         res.send({
           day,
