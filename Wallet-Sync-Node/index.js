28b84255bf566b2e3f65a3deb139c3d1d0fb2457
Ashwin-DevAsh <2022ashwin@gmail.com>
2020-07-30 01:18:44 +0530

    node sync

---
+++ Wallet-Sync-Node/index.js
@@ -0,0 +1,18 @@
+const app = require("express")();
+const server = require("http").createServer(app);
+const io = require("socket.io")(server);
+const postgres = require("./Database");
+
+io.on("connection", (client) => {
+  console.log("connected ", client.id);
+
+  client.on("getInformation", (data) => {
+    var id = data["id"];
+    var token = data["fcmToken"];
+    console.log(id, token);
+  });
+});
+
+server.listen(7000, () => {
+  console.log("Listing at 7000");
+});
