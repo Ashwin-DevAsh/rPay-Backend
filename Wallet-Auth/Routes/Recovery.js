798909ea3c40c8e9d48eb435eb1a3323beb68f2f
Ashwin-DevAsh <2022ashwin@gmail.com>
2020-07-30 21:58:20 +0530

    email otp

--- Wallet-Auth/Routes/Recovery.js
+++ Wallet-Auth/Routes/Recovery.js
@@ -9,65 +9,67 @@ var sendOtp = (req, res, OtpObject) => {
   var emailID = req.query["emailID"];
   var otpNumber = Math.floor(1000 + Math.random() * 9000);
 
-  //   jwt.verify(req.get("token"), process.env.PRIVATE_KEY, function (
-  //     err,
-  //     decoded
-  //   ) {
-  // if (err) {
-  //   console.log("jwt error = ", err);
-  //   res.status(200).send({ message: "error" });
-  //   return;
-  // } else {
-  console.log("Recovery password...");
-  if (emailID) {
-    OtpObject.deleteMany({ emailID: emailID })
-      .exec()
-      .then(() => {
-        var emailTransactionalApi = new api.TransactionalEmailApi(
-          process.env.OTP_USERNAME,
-          process.env.OTP_API_KEY
-        );
+  jwt.verify(req.get("token"), process.env.PRIVATE_KEY, function (
+    err,
+    decoded
+  ) {
+    if (err) {
+      console.log("jwt error = ", err);
+      res.status(200).send({ message: "error" });
+      return;
+    } else {
+      console.log("Recovery password...");
+      if (emailID) {
+        OtpObject.deleteMany({ emailID: emailID })
+          .exec()
+          .then(() => {
+            var emailTransactionalApi = new api.TransactionalEmailApi(
+              process.env.OTP_USERNAME,
+              process.env.OTP_API_KEY
+            );
 
-        var emailRecipient = new api.EmailRecipient();
-        emailRecipient.email = emailID;
-        emailRecipient.name = "Rpay";
+            var emailRecipient = new api.EmailRecipient();
+            emailRecipient.email = emailID;
+            emailRecipient.name = "Rpay";
 
-        var emailFrom = new api.EmailFrom();
-        emailFrom.emailAddressId = 11297;
-        emailFrom.name = "Rpay";
+            var emailFrom = new api.EmailFrom();
+            emailFrom.emailAddressId = 11297;
+            emailFrom.name = "Rpay";
 
-        var email = new api.Email();
+            var email = new api.Email();
 
-        email.to = [emailRecipient];
-        email.from = emailFrom;
-        email.subject = `Password Recovery`;
-        email.body = `Rpay never calls you asking for otp. Sharing it with anyone gives them full access to your Rpay wallet. Your Recovery OTP is ${otpNumber}`;
+            email.to = [emailRecipient];
+            email.from = emailFrom;
+            email.subject = `Password Recovery`;
+            email.body = `Rpay never calls you asking for otp. Sharing it with anyone gives them full access to your Rpay wallet. Your Recovery OTP is ${otpNumber}`;
 
-        emailTransactionalApi
-          .emailSendPost(email)
-          .then(function (response) {
-            console.log(response.body);
-            const otpObject = new OtpObject({
-              emailID,
-              otp: otpNumber,
-              verified: false,
-            });
-            otpObject.save();
-            res.json([{ message: "done" }]);
+            emailTransactionalApi
+              .emailSendPost(email)
+              .then(function (response) {
+                console.log(response.body);
+                const otpObject = new OtpObject({
+                  emailID,
+                  otp: otpNumber,
+                  verified: false,
+                });
+                otpObject.save();
+                res.json([{ message: "done" }]);
+              })
+              .catch(function (err) {
+                console.log("sms error = ", err);
+                res.json([
+                  { message: "failed", err, text: "Unable to send sms" },
+                ]);
+              });
           })
-          .catch(function (err) {
-            console.log("sms error = ", err);
-            res.json([{ message: "failed", err, text: "Unable to send sms" }]);
+          .catch((e) => {
+            res.json([{ message: "failed", err: e, text: "delete otp err" }]);
           });
-      })
-      .catch((e) => {
-        res.json([{ message: "failed", err: e, text: "delete otp err" }]);
-      });
-  } else {
-    res.json([{ message: "failed" }]);
-  }
-  // }
-  //   });
+      } else {
+        res.json([{ message: "failed" }]);
+      }
+    }
+  });
 };
 
 module.exports = app;
