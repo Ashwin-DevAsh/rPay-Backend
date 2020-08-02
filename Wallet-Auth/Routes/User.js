baecdaa663ca00518067382f9a3053803d49cb8b
Ashwin-DevAsh <2022ashwin@gmail.com>
2020-08-02 13:06:10 +0530

    Add profile picture

--- Wallet-Auth/Routes/User.js
+++ Wallet-Auth/Routes/User.js
@@ -8,7 +8,7 @@ const postgres = require("../Database/postgresql");
 
 var storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, (__dirname, "/uploads/"));
+    cb(null, path.join(__dirname, "/uploads/"));
   },
   filename: function (req, file, cb) {
     cb(null, file.originalname);
