9e15675176a79b7b705f2105f0d244d549e6a8cc
Ashwin-DevAsh <2022ashwin@gmail.com>
2020-08-02 14:02:54 +0530

    Add profile picture

--- Wallet-Auth/Routes/UploadPictures.js
+++ Wallet-Auth/Routes/UploadPictures.js
@@ -1,25 +1,24 @@
-const aws = require('aws-sdk'),
-const multerS3 = require('multer-s3');
+const aws = require("aws-sdk");
+const multerS3 = require("multer-s3");
 const multer = require("multer");
 const path = require("path");
 
-
 aws.config.update({
-    secretAccessKey: process.env.AWSSecretKey,
-    accessKeyId: process.env.AWSAccessKeyId,
-    region: 'us-east-2'
+  secretAccessKey: process.env.AWSSecretKey,
+  accessKeyId: process.env.AWSAccessKeyId,
+  region: "us-east-2",
 });
 
 var upload = multer({
-    storage: multerS3({
-        s3: s3,
-        bucket: 'rec-wallet-profile-pictures',
-        key: function (req, file, cb) {
-            console.log(file);
-            cb(null, file.originalname);
-        }
-    })
-}).single('avatar');
+  storage: multerS3({
+    s3: s3,
+    bucket: "rec-wallet-profile-pictures",
+    key: function (req, file, cb) {
+      console.log(file);
+      cb(null, file.originalname);
+    },
+  }),
+}).single("avatar");
 
 app.post("/addProfilePicture", (req, res) => {
   upload(req, res, (err) => {
@@ -33,4 +32,4 @@ app.post("/addProfilePicture", (req, res) => {
   });
 });
 
-module.exports=app
+module.exports = app;
