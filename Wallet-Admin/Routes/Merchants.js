2c76be29350c20c3f7a3718a3e70cc377c55bc1e
Ashwin-DevAsh <2022ashwin@gmail.com>
2020-08-08 01:19:44 +0530

    change merchant password

--- Wallet-Admin/Routes/Merchants.js
+++ Wallet-Admin/Routes/Merchants.js
@@ -25,61 +25,6 @@ app.get("/getMerchants", (req, res) => {
     });
 });
 
-app.post("/changeMerchantPassword", (req, res) => {
-  console.log(req.get("token"));
-
-  jwt.verify(req.get("token"), process.env.PRIVATE_KEY, function (
-    err,
-    decoded
-  ) {
-    if (err) {
-      console.log(err);
-      res.status(200).send({ message: "error" });
-      return;
-    } else {
-      console.log("Changing password...");
-      var data = req.body;
-      console.log(data);
-      if (!data.id || !data.oldPassword || !data.newPassword) {
-        res.status(200).send({ message: "error" });
-        return;
-      }
-
-      Users.findOne({ id: data.id })
-        .exec()
-        .then((docs) => {
-          console.log("data = ", docs);
-          if (docs == null) {
-            res.status(200).send({ message: "error" });
-            return;
-          }
-
-          if (docs.password == data.oldPassword) {
-            Users.findOneAndUpdate(
-              { id: data.id },
-              { password: data.newPassword },
-              (err, doc) => {
-                if (err) {
-                  console.log(err);
-                  res.status(200).send({ message: "error" });
-                  return;
-                } else {
-                  res.status(200).send({ message: "done" });
-                  return;
-                }
-              }
-            );
-          } else {
-            console.log("data = ", null);
-
-            res.status(200).send({ message: "error" });
-            return;
-          }
-        });
-    }
-  });
-});
-
 app.post("/updateMerchantStatus", (req, res) => {
   var id = req.body.id;
   var status = req.body.status;
