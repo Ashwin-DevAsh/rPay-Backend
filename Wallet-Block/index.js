5d085da62b83b627c9a35f5c52dc297d0d63a0dc
Ashwin-DevAsh <2022ashwin@gmail.com>
2020-08-08 15:43:19 +0530

    added in docker-compose

--- Wallet-Block/index.js
+++ Wallet-Block/index.js
@@ -3,6 +3,13 @@ const redisClient = require("./Database/Connections/redisConnection");
 
 const app = express();
 
+redisClient.set("demo", "1", (err, replay) => {
+  console.log(err, replay);
+  redisClient.get("demo", (err, replay) => {
+    console.log(err, replay);
+  });
+});
+
 app.get("/", (req, res) => {
   res.send({ message: "wellcome to rec-wallet block" });
 });
