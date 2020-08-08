fc4cd72022b819fb8df8cbf5af114d618b1e20d2
Ashwin-DevAsh <2022ashwin@gmail.com>
2020-08-08 18:21:29 +0530

    added in docker-compose

--- Wallet-Block/Routes/Transactions.js
+++ Wallet-Block/Routes/Transactions.js
@@ -4,7 +4,7 @@ const app = require("express").Router();
 
 app.post("/addTransactionBlock", (req, res) => {
   console.log("Adding transaction block....");
-  console.log(req);
+  console.log(req.body);
   res.send({ message: "done" });
 });
 
--- Wallet-Block/index.js
+++ Wallet-Block/index.js
@@ -1,8 +1,20 @@
 const express = require("express");
 const redisClient = require("./Database/Connections/redisConnection");
 const transactionBlocks = require("./Routes/Transactions");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 
 const app = express();
+
+var corsOptions = {
+  origin: "*",
+  optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+};
+
+app.use(cors(corsOptions));
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
   res.send({ message: "wellcome to rec-wallet block" });
