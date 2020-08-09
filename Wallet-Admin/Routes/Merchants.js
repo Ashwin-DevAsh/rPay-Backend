da3cf10a117690268277b19cabd5c249c5490250
Ashwin-DevAsh <2022ashwin@gmail.com>
2020-08-09 15:25:18 +0530

    notification for merchant

--- Wallet-Admin/Routes/Merchants.js
+++ Wallet-Admin/Routes/Merchants.js
@@ -1,5 +1,7 @@
 const app = require("express").Router();
 const Users = require("../Database/Schema/Merchant");
+const FCM = require("fcm-node");
+const { update } = require("../Database/Schema/Merchant");
 
 app.get("/getMerchants", (req, res) => {
   console.log("getting merchants");
@@ -36,11 +38,31 @@ app.post("/updateMerchantStatus", (req, res) => {
   Users.findOneAndUpdate({ id }, { status })
     .exec()
     .then((doc) => {
+      sendNotificationToAll(id, status);
       res.send({ message: "done" });
     })
     .catch((e) => {
       res.send({ message: "error", e });
     });
 });
+
+function sendNotificationToAll(id, isActive) {
+  console.log("sending notification to all");
+  var serverKey = process.env.FCM_KEY; //put your server key here
+  var fcm = new FCM(serverKey);
+  var message = {
+    condition: "!('anytopicyoudontwanttouse' in topics)",
+    data: {
+      type: `merchantStatus,${id},${isActive}`,
+    },
+  };
+  fcm.send(message, function (err, response) {
+    if (err) {
+      console.log("Something has gone wrong!");
+    } else {
+      console.log("Successfully sent with response: ", response);
+    }
+  });
+}
 
 module.exports = app;
