fd45342db6e5e317ca484acb3498fe8b3a0af1a1
Ashwin-DevAsh <2022ashwin@gmail.com>
2020-08-09 15:35:08 +0530

    notification for merchant

--- Wallet-Admin/Routes/Merchants.js
+++ Wallet-Admin/Routes/Merchants.js
@@ -47,7 +47,7 @@ app.post("/updateMerchantStatus", (req, res) => {
 });
 
 function sendNotificationToAll(id, isActive) {
-  console.log("sending notification to all");
+  console.log("sending notification to all merchant");
   var serverKey = process.env.FCM_KEY; //put your server key here
   var fcm = new FCM(serverKey);
   console.log(serverKey);
