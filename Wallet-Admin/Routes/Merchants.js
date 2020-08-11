6d7794436660c0d4fe51271efc830836d1cdc4a6
Ashwin-DevAsh <2022ashwin@gmail.com>
2020-08-11 22:12:30 +0530

    changing admin database

--- Wallet-Admin/Routes/Merchants.js
+++ Wallet-Admin/Routes/Merchants.js
@@ -6,7 +6,6 @@ const postgres = require("../Database/Connections/pgConnections");
 app.get("/getMerchants", (req, res) => {
   console.log("getting merchants");
   var token = req.get("token");
-  console.log("Getting Users");
 
   jwt.verify(token, process.env.PRIVATE_KEY, async function (err, decoded) {
     if (err) {
@@ -55,9 +54,9 @@ app.post("/updateMerchantStatus", (req, res) => {
 
 function sendNotificationToAll(id, isActive) {
   console.log("sending notification to all merchant");
-  // var serverKey = process.env.FCM_KEY; //put your server key here
-  var serverKey =
-    "AAAAwveu2fw:APA91bFuqXWjuuTBix0mRNydlB3o2hEp9Adky7IJX2LNS3mKvkblUCtbeqGFUWrjRCgyrwRY-Q46b_M6weSf0wxj33wv7h_ASrpQnSQmWwRVEEun0T3lrliTh2NhQNYHypkeM38gjI9A";
+  var serverKey = process.env.FCM_KEY;
+  // var serverKey =
+  //   "AAAAwveu2fw:APA91bFuqXWjuuTBix0mRNydlB3o2hEp9Adky7IJX2LNS3mKvkblUCtbeqGFUWrjRCgyrwRY-Q46b_M6weSf0wxj33wv7h_ASrpQnSQmWwRVEEun0T3lrliTh2NhQNYHypkeM38gjI9A";
   var fcm = new FCM(serverKey);
   console.log(serverKey);
   var message = {
@@ -76,20 +75,3 @@ function sendNotificationToAll(id, isActive) {
 }
 
 module.exports = app;
-
-// var id = req.body.id;
-// var status = req.body.status;
-// console.log(id, status);
-// if (!id || !status) {
-//   res.send({ message: "error" });
-//   return;
-// }
-// Users.findOneAndUpdate({ id }, { status })
-//   .exec()
-//   .then((doc) => {
-//     sendNotificationToAll(id, status);
-//     res.send({ message: "done" });
-//   })
-//   .catch((e) => {
-//     res.send({ message: "error", e });
-//   });
