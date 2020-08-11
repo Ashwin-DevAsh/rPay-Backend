72857def5649a829c5e669cf766c96704621d8eb
Ashwin-DevAsh <2022ashwin@gmail.com>
2020-08-11 20:37:34 +0530

    fix admin

--- Wallet-Admin/Routes/Users.js
+++ Wallet-Admin/Routes/Users.js
@@ -1,24 +1,21 @@
 const app = require("express").Router();
 const jwt = require("jsonwebtoken");
 const postgres = require("../Database/Connections/pgConnections");
-const Users = require("../Database/Schema/Users");
 
 app.get("/getUsers", (req, res) => {
   var token = req.get("token");
   console.log("Getting Users");
 
-  jwt.verify(token, process.env.PRIVATE_KEY, function (err, decoded) {
+  jwt.verify(token, process.env.PRIVATE_KEY, async function (err, decoded) {
     if (err) {
       res.send({ message: "error", err });
     } else {
-      Users.find({})
-        .exec()
-        .then((doc) => {
-          res.send(doc);
-        })
-        .catch((e) => {
-          res.send({ err: e });
-        });
+      try {
+        var users = (await postgres.query("select * from users")).rows;
+        res.send(users);
+      } catch (err) {
+        res.send({ err });
+      }
     }
   });
 });
