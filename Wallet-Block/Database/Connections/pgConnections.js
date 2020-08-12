601fab15990f2ad49f34c4be4c8f91665a60330d
Ashwin-DevAsh <2022ashwin@gmail.com>
2020-08-12 06:12:54 +0530

    add block transaction

--- Wallet-Block/Database/Connections/pgConnections.js
+++ Wallet-Block/Database/Connections/pgConnections.js
@@ -7,7 +7,7 @@ const client = new Client({
   host: "database",
   port: 5432,
   user: "postgres",
-  password: process.env.POSTGRES_PASSWORD,
+  password: "2017PASS",
   database: "Rec_Wallet",
 });
 
