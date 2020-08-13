73cd1da03d74fe531cf3c715ec7b7daf63e2da21
Ashwin-DevAsh <2022ashwin@gmail.com>
2020-08-14 03:12:55 +0530

    delect bank accounts

--- Wallet-Profile/Routes/BankAccounts.js
+++ Wallet-Profile/Routes/BankAccounts.js
@@ -3,8 +3,15 @@ const jwt = require("jsonwebtoken");
 const postgres = require("../Database/postgresql");
 
 app.post("/addBankAccount", (req, res) => addBankAccount(req, res, "users"));
+app.post("/deleteBankAccount", (req, res) =>
+  deleteBankAccount(req, res, "users")
+);
+
 app.post("/addBankAccountMerchant", (req, res) =>
   addBankAccount(req, res, "merchants")
+);
+app.post("/deleteBankAccountMerchant", (req, res) =>
+  deleteBankAccount(req, res, "merchants")
 );
 
 function addBankAccount(req, res, tableName) {
@@ -23,8 +30,6 @@ function addBankAccount(req, res, tableName) {
     var accountNumber = req.body.accountNumber;
     var ifsc = req.body.ifsc;
     var bankName = req.body.bankName;
-
-    console.log(req.body);
 
     if (!id || !holderName || !accountNumber || !ifsc || !bankName) {
       res.send({ message: "failed" });
@@ -46,4 +51,28 @@ function addBankAccount(req, res, tableName) {
   });
 }
 
+function deleteBankAccount(req, res, tableName) {
+  jwt.verify(req.get("token"), process.env.PRIVATE_KEY, async function (
+    err,
+    decoded
+  ) {
+    if (err) {
+      console.log(err);
+      res.send({ message: "failed" });
+      return;
+    }
+
+    var id = req.body.id;
+    var bankAccounts = req.body.bankAccounts;
+
+    if (!id || !bankAccounts) {
+      res.send({ message: "failed" });
+      return;
+    }
+
+    await postgres.query(`update ${tableName} set AccountInfo = $1`, [
+      bankAccounts,
+    ]);
+  });
+}
 module.exports = app;
