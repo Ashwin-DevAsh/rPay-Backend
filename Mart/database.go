3254804842ea08b8b31493ed0cd205d1a09e1202
Ashwin-DevAsh <2022ashwin@gmail.com>
2020-09-18 17:02:39 +0530

    added mart

---
+++ Mart/database.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"time"
+
+	_ "github.com/lib/pq"
+)
+
+// Connect ...
+func Connect() *sql.DB {
+	const (
+		host     = "status-database"
+		port     = 5432
+		user     = "postgres"
+		password = "2017PASS"
+		dbname   = "Rec_Wallet"
+	)
+
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
+		"password=%s dbname=%s sslmode=disable",
+		host, port, user, password, dbname)
+	db, err := sql.Open("postgres", psqlInfo)
+
+	if err != nil {
+		log.Println(err)
+	}
+
+	return db
+}
+
+func doTransaction(db *sql.DB, from string, to string, toName string, amount uint64) bool {
+	_, errFrom := db.Exec("update amount set balance = balance - $1 where id = $2", amount, from)
+
+	if errFrom != nil {
+		log.Println(errFrom)
+		return false
+	}
+
+	_, errTo := db.Exec("update amount set balance = balance + $1 where id = $2", amount, to)
+
+	if errTo != nil {
+		db.Exec("update amount set balance = balance + $1 where id = $2", amount, from)
+		return false
+	}
+
+	dt := time.Now()
+
+	_, errTrans := db.Exec("insert into transactions(transactionTime,fromID,toID,toName,amount) values($1,$2,$3,$4,$5)", dt.Format("01-02-2006 15:04:05"), from, to, toName, amount)
+
+	if errTrans != nil {
+		db.Exec("update amount set balance = balance + $1 where id = $2", amount, from)
+		db.Exec("update amount set balance = balance - $1 where id = $2", amount, to)
+		return false
+	}
+
+	return true
+}
