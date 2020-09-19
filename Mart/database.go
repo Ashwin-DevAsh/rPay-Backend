0389195172364cec3bc8c450e6b7a66659807892
Ashwin-DevAsh <2022ashwin@gmail.com>
2020-09-19 16:56:42 +0530

    order database

--- Mart/database.go
+++ Mart/database.go
@@ -54,7 +54,7 @@ func doOrder(db *sql.DB, orderData OrderData, transactionID *uint64, transaction
 	fromJson, _ := json.Marshal(&orderData.TransactionData.From)
 	toJson, _ := json.Marshal(&orderData.TransactionData.To)
 	Amount, _ := strconv.ParseUint(orderData.TransactionData.Amount, 10, 64)
-	products,_ := json.Marshal(&orderData.Products)   
+	_,_ := json.Marshal(&orderData.Products)   
 
 
 	row, err := db.Query("select * from amount where id=$1", orderData.TransactionData.From.Id)
