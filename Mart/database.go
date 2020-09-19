eb0585767eeb3ede29f4ae867fe22c1910fb59db
Ashwin-DevAsh <2022ashwin@gmail.com>
2020-09-19 17:53:33 +0530

    order database

--- Mart/database.go
+++ Mart/database.go
@@ -126,13 +126,11 @@ func doOrder(db *sql.DB, orderData OrderData, transactionID *uint64, transaction
 	order, errTrans :=
 		tx.Query(`insert
 		           into orders(
-					   status,
-					   orderdBy,
-					   timestamp,
-					   products,
-					   paymentMetadata)
-				    values('pending',$1,$2,$3,$4) returning *`,
-			toJson, transactionTime, products,fromJson)
+					
+					   amount
+					 )
+				    values($1) returning *`,
+			Amount)
 
 	if errTrans!=nil{
 		tx.Rollback()
