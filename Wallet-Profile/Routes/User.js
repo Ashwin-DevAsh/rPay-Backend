f9f84e3cc32fa89c20ba93722f3321173edfad89
Ashwin-DevAsh <2022ashwin@gmail.com>
2020-10-31 14:52:47 +0530

    get Users with contacts

--- Wallet-Profile/Routes/User.js
+++ Wallet-Profile/Routes/User.js
@@ -137,7 +137,7 @@ var getUsersWithContacts = async(postgres,contacts,req,res)=>{
   console.log("select name,number,email,id from users where number = Any("+contacts+")")
   try {
     var result = (
-      await postgres.query("select name,number,email,id from users where number in Array"+contacts)
+      await postgres.query("select name,number,email,id from users where number = Any(Array"+contacts+")")
     ).rows;
     res.send(result);
   } catch (err) {
