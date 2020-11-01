7b916e6d082985911420bfbf57831f33090aa88d
Ashwin-DevAsh <2022ashwin@gmail.com>
2020-11-01 17:56:45 +0530

    transactions

--- Transaction/Routes/status.js
+++ Transaction/Routes/status.js
@@ -42,8 +42,7 @@ async function getTransactionsBetweenObjects(postgres, req, res) {
 						   where 
 								(cast(fromMetadata->>'id' as varchar) = $1 or cast(fromMetadata->>'id' as varchar) = $2) 
 								     and 
-								(cast(toMetadata->>'id' as varchar) = $1 or cast(toMetadata->>'id' as varchar) = $2))
-						   order by TimeStamp`,
+								(cast(toMetadata->>'id' as varchar) = $1 or cast(toMetadata->>'id' as varchar) = $2))`,
         [fromID, toID]
       )
     ).rows;
