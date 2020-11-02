8021f95b5410f64c3344b832e5e1243ab4948360
Ashwin-DevAsh <2022ashwin@gmail.com>
2020-11-02 12:11:31 +0530

    notification for profile picture

--- Profile/Routes/UploadPictures.js
+++ Profile/Routes/UploadPictures.js
@@ -3,6 +3,7 @@ const multerS3 = require("multer-s3");
 const multer = require("multer");
 const jwt = require("jsonwebtoken");
 const app = require("express").Router();
+const FCM = require("fcm-node");
 
 aws.config.update({
   secretAccessKey: process.env.AWS_SEC_KEY,
@@ -26,39 +27,53 @@ var upload = multer({
   }),
 }).single("profilePicture");
 
-app.post("/addProfilePicture/:id", async(req, res) => {
-
-  await addProfilePicture(req,res)
-
-
+app.post("/addProfilePicture/:id", async (req, res) => {
+  await addProfilePicture(req, res);
 });
 
-var addProfilePicture =  async (req,res)=>{
+var addProfilePicture = async (req, res) => {
+  try {
+    var id = await jwt.verify(req.get("token"), process.env.PRIVATE_KEY).id;
+  } catch (e) {
+    console.log(e);
+    res.send({ message: "error" });
+    return;
+  }
 
-
-  try{
-    var decoded = await jwt.verify(req.get("token"), process.env.PRIVATE_KEY)
-   }catch(e){
-     console.log(e)
-     res.send({ message: "error" });
-     return
-   }
-
-      upload(req, res, (err, result) => {
-        if (err) {
-          console.log(err);
-          res.send({ message: "error", err });
-        } else if (!req.file) {
-          res.send({ message: "error", err: "Invalid file" });
-        } else {
-          res.send({
-            message: "done",
-            result: result,
-            path: "path = " + this.path,
-          });
-        }
+  upload(req, res, (err, result) => {
+    if (err) {
+      console.log(err);
+      res.send({ message: "error", err });
+    } else if (!req.file) {
+      res.send({ message: "error", err: "Invalid file" });
+    } else {
+      res.send({
+        message: "done",
+        result: result,
+        path: "path = " + this.path,
       });
-}
+      sendNotificationToAll(req.params.id);
+    }
+  });
+};
 
+function sendNotificationToAll(id) {
+  var serverKey = process.env.FCM_KEY;
+  var fcm = new FCM(serverKey);
+  console.log(serverKey);
+  var message = {
+    to: "/topics/all",
+    data: {
+      type: `updateProfilePicture,${id}`,
+    },
+  };
+  fcm.send(message, function (err, response) {
+    if (err) {
+      console.log("Something has gone wrong!");
+    } else {
+      console.log("Successfully sent with response: ", response);
+    }
+  });
+}
 
 module.exports = app;
