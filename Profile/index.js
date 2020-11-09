0ea7dd4312e59f3bd5df3ac0499ba17ca9dd6704
Ashwin-DevAsh <2022ashwin@gmail.com>
2020-11-10 02:39:25 +0530

    added ngnix proxy

--- Profile/index.js
+++ Profile/index.js
@@ -13,6 +13,15 @@ const init = require("./Routes/init");
 
 const app = express();
 
+const cors = require("cors");
+
+var corsOptions = {
+  origin: "*",
+  optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+};
+
+app.use(cors(corsOptions));
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
