8a3131ab1a5453e5ae4ad59db9f2494523e8299b
Ashwin-DevAsh <2022ashwin@gmail.com>
2020-12-02 15:45:35 +0530

    console

---
+++ Console/src/app/login/LoginService.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@angular/core';
+import { JwtHelperService } from '@auth0/angular-jwt';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class LoginService {
+  constructor() {}
+
+  public url: String = 'http://3.15.27.159:4500/';
+  public endPoint: String = 'login';
+  public userName: String = localStorage.getItem('userName');
+  public email: String = localStorage.getItem('email');
+  public phoneNumber: String = localStorage.getItem('phoneNumber');
+  public imageURL: String = localStorage.getItem('imageURL');
+  public token: string = localStorage.getItem('token');
+
+  public isSessionExist(): Boolean {
+    var jwtHelper = new JwtHelperService();
+    try {
+      var decodedToken = jwtHelper.decodeToken(this.token);
+      if (!decodedToken.name) {
+        return false;
+      }
+    } catch {
+      return false;
+    }
+
+    console.log(this.token);
+    return !jwtHelper.isTokenExpired(this.token);
+  }
+}
+
+interface Result {
+  message: String;
+}
