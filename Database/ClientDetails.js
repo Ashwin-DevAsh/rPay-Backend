4582c31b3a16d3f4653a2148829ff98ec68370de
Ashwin-DevAsh <2022ashwin@gmail.com>
2020-12-02 12:34:08 +0530

    env

--- Database/ClientDetails.js
+++ Database/ClientDetails.js
@@ -1,6 +1,6 @@
 module.exports = {
   host: process.env.POSTGRES_URL,
-  port: 5432,
+  port: 5433,
   user: process.env.POSTGRES_USER,
   password: process.env.POSTGRES_PASSWORD,
   database: process.env.POSTGRES_DATABASE,
