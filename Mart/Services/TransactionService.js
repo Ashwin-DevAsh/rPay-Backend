b4d939acb545efb94480140bfcd49da20bb567fe
Ashwin-DevAsh <2022ashwin@gmail.com>
2021-02-07 04:49:04 +0530

    rmartPayment

--- Mart/Services/TransactionService.js
+++ Mart/Services/TransactionService.js
@@ -90,9 +90,10 @@ module.exports = class TranslationService {
 
   payToMerchant = async (from, merchantID, transactionID, amount) => {
     var postgres = await this.pool.connect();
-    var toAmmount = await postgres.query(
-      "select * from users where id=$1 for update",
-      [merchantID]
+    var toAmmount = (
+      await postgres.query("select * from users where id=$1 for update", [
+        merchantID,
+      ])
     ).rows[0];
 
     console.log(toAmmount);
