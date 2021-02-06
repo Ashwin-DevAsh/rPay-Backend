a0490bd9f293b7aa0af72eb7d89221a0a74c617f
Ashwin-DevAsh <2022ashwin@gmail.com>
2021-02-07 05:00:08 +0530

    rmartPayment

--- Mart/Routes/orders.js
+++ Mart/Routes/orders.js
@@ -1,8 +1,9 @@
 const app = require("express").Router();
+const Auth = require("../Services/Auth");
 const OrdersController = require("../Controllers/ordersController");
 
 var ordersController = new OrdersController();
 
-app.post("/makeOrder", ordersController.makeOrder);
+app.post("/makeOrder", new Auth().isTransAuth, ordersController.makeOrder);
 
 module.exports = app;
