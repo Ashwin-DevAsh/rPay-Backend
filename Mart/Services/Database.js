488a62fa6087be052b42cfb0291c02774f655dd0
Ashwin-DevAsh <2022ashwin@gmail.com>
2021-02-07 12:03:29 +0530

    rmartPayment

--- Mart/Services/Database.js
+++ Mart/Services/Database.js
@@ -150,6 +150,7 @@ module.exports = class Database {
       var orders = (
         await postgres.query(`select * from orders  where orederid = $1`, [id])
       ).rows[0];
+      console.log(orders);
       if (!orders) {
         postgres.release();
         return "invalid token";
